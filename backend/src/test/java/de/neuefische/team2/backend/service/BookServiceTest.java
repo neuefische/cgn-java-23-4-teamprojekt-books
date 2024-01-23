2e4cde79765a5c3dfa178abda006cbc3ec23f5ee
Alexandra Dernova <alexder@gmx.net>
2024-01-23 16:42:36 +0100

    Add Frontend

---
+++ backend/src/test/java/de/neuefische/team2/backend/service/BookServiceTest.java
@@ -0,0 +1,38 @@
+package de.neuefische.team2.backend.service;
+
+import de.neuefische.team2.backend.models.Book;
+import de.neuefische.team2.backend.repos.BooksRepo;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookServiceTest {
+
+    BooksRepo booksRepo = Mockito.mock(BooksRepo.class);
+
+    @Test
+    public void getBooksTest_returnListOfAllBook(){
+        //GIVEN
+        Mockito.when(booksRepo.findAll()).thenReturn(List.of(
+                new Book("1", "Harry Potter und der Stein der Weisen", "J.K. Rowling"),
+                new Book("2", "Harry Potter und die Kammer des Schreckens", "J.K. Rowling")
+        ));
+
+        BookService bookService = new BookService(booksRepo);
+
+        //WHEN
+        List<Book> actual = bookService.getBooks();
+
+        //THEN
+        assertEquals(List.of(
+                new Book("1", "Harry Potter und der Stein der Weisen", "J.K. Rowling"),
+                new Book("2", "Harry Potter und die Kammer des Schreckens", "J.K. Rowling")
+        ), actual);
+
+        Mockito.verify(booksRepo, Mockito.times(1)).findAll();
+        Mockito.verifyNoMoreInteractions(booksRepo);
+    }
+
+}
