e850ae4422badff5abd3ad7aa26e4cde3fdb2121
AslonBadridinovGH <151860764+AslonBadridinovGH@users.noreply.github.com>
2024-01-24 10:41:58 +0100

    test for add method

---
+++ backend/src/main/java/de/neuefische/team2/backend/service/IdService.java
@@ -0,0 +1,12 @@
+package de.neuefische.team2.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class IdService {
+    public String newId(){
+        return UUID.randomUUID().toString();
+    }
+}
--- backend/src/test/java/de/neuefische/team2/backend/service/BookServiceTest.java
+++ backend/src/test/java/de/neuefische/team2/backend/service/BookServiceTest.java
@@ -1,6 +1,7 @@
 package de.neuefische.team2.backend.service;
 
 import de.neuefische.team2.backend.models.Book;
+import de.neuefische.team2.backend.models.BookDto;
 import de.neuefische.team2.backend.repos.BooksRepo;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -11,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BookServiceTest {
 
     BooksRepo booksRepo = Mockito.mock(BooksRepo.class);
+    IdService idService = Mockito.mock(IdService.class);
+
 
     @Test
     public void getBooksTest_returnListOfAllBook(){
@@ -20,7 +23,7 @@ public class BookServiceTest {
                 new Book("2", "Harry Potter und die Kammer des Schreckens", "J.K. Rowling")
         ));
 
-        BookService bookService = new BookService(booksRepo);
+        BookService bookService = new BookService(booksRepo, idService);
 
         //WHEN
         List<Book> actual = bookService.getBooks();
@@ -33,6 +36,30 @@ public class BookServiceTest {
 
         Mockito.verify(booksRepo, Mockito.times(1)).findAll();
         Mockito.verifyNoMoreInteractions(booksRepo);
+    }
+
+    @Test
+    public void addBookTest_returnBook(){
+
+        BookDto bookDto = new BookDto("Harry Potter und der Stein der Weisen", "J.K. Rowling");
+        Book book = new Book("test-id","Harry Potter und der Stein der Weisen", "J.K. Rowling");
+
+        // GIVEN
+        Mockito.when(booksRepo.save(book)).thenReturn(book);
+        Mockito.when(idService.newId()).thenReturn("test-id");
+
+        BookService bookService = new BookService(booksRepo, idService);
+
+        // WHEN
+        Book actual = bookService.addBook(bookDto);
+
+        // THEN
+        Mockito.verify(booksRepo).save(book);
+        Mockito.verify(idService).newId();
+
+        Book expected = new Book("test-id","Harry Potter und der Stein der Weisen", "J.K. Rowling");
+        assertEquals(expected, actual);
+
     }
 
 }
