d42d36392760c3cedc086c0baf43f78760bd1ffd
paulkreft <paulykreft@gmail.com>
2024-01-24 09:30:03 +0100

    create book edit form

--- frontend/src/components/edit-book.tsx
+++ frontend/src/components/edit-book.tsx
@@ -1,5 +1,5 @@
 import {Book} from "../types/Book.ts";
-import React from "react";
+import React, {ChangeEvent, FormEvent, useState} from "react";
 import {useParams} from "react-router-dom";
 
 type EditBookProps = {
@@ -12,9 +12,41 @@ export const EditBook: React.FC<EditBookProps> = ({books}) => {
 
     const book: Book | undefined = books.find(book => book.id === id);
 
+    const [title, setTitle] = useState<string>(book?.title || "")
+    const [author, setAuthor] = useState<string>(book?.author || "")
+
+    const onTitleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setTitle(event.target.value)
+    }
+
+    const onAuthorChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        setAuthor(event.target.value)
+    }
+
+    const editBook = (event: FormEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        console.log("submit")
+        console.log(title)
+        console.log(author)
+    }
+
     return (
         <div className="book-detail">
-            <div className="book">{book?.title}</div>
+            <div className="book">
+                <form onSubmit={editBook}>
+                    <div>
+
+                        <div>Title</div>
+                        <input name="title" value={title} type="text" onChange={onTitleChange} placeholder="Title..."/>
+                    </div>
+                    <div>
+                        <div>Author</div>
+                        <input name="author" value={author} type="text" onChange={onAuthorChange}
+                               placeholder="Author..."/>
+                    </div>
+                    <button type="submit">Edit book</button>
+                </form>
+            </div>
         </div>
     );
 
