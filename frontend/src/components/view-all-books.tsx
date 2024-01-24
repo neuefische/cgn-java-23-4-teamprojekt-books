42c82ef9d92900680d0c86fc1078512745489b5c
Alexandra Dernova <alexder@gmx.net>
2024-01-24 09:41:46 +0100

    Restructuring: type View All Books Props from view-all-books.tsx - > global TypeBooksList.tsx(in folder types)

--- frontend/src/components/view-all-books.tsx
+++ frontend/src/components/view-all-books.tsx
@@ -1,14 +1,11 @@
-import {Book} from "../Book.ts";
-
-type ViewAllBooksProps = {
-    books: Book[]
-}
+import {ViewAllBooksProps} from "../types/TypeBooksList.tsx";
+import {Link} from "react-router-dom";
 
 export const ViewAllBooks: React.FC<ViewAllBooksProps> = ({books}) => {
 
     return (
         <div className="books">
-            {books.map(book => (<div key={book.id} className="book">{book.title}</div>))}
+            {books.map(book => (<Link to={`/${book.id}`} key={book.id} className="book">{book.title}</Link>))}
         </div>
     );
 
---
+++ frontend/src/types/TypeBooksList.tsx
@@ -0,0 +1,5 @@
+import {Book} from "../Book.ts";
+
+export type ViewAllBooksProps = {
+    books: Book[]
+}
