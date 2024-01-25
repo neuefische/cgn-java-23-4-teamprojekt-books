43fed1c8aba0640e69038cfe5f866a6e732bb380
vanessabock <vanessabock54@gmail.com>
2024-01-25 12:49:29 +0100

    Extend book model and dto

--- backend/src/main/java/de/neuefische/team2/backend/models/Book.java
+++ backend/src/main/java/de/neuefische/team2/backend/models/Book.java
@@ -1,13 +1,15 @@
 package de.neuefische.team2.backend.models;
-
-import lombok.With;
 import org.springframework.data.annotation.Id;
 
 public record Book(
         @Id
         String id,
         String title,
-        String author
-                    ) {
+        String author,
+        String isbn,
+        String genre,
+        String publicationDate,
+        String imageURL
+) {
 
 }
--- backend/src/main/java/de/neuefische/team2/backend/models/BookDto.java
+++ backend/src/main/java/de/neuefische/team2/backend/models/BookDto.java
@@ -1,12 +1,12 @@
 package de.neuefische.team2.backend.models;
 
-import lombok.With;
-
 public record BookDto(
         String title,
-        String author
-/*        String genre,
-        String publication_date*/
+        String author,
+        String isbn,
+        String genre,
+        String publicationDate,
+        String imageURL
         )
 {
 }
