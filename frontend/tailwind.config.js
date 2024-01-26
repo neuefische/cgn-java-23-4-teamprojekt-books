69f3fed194304af213787dcc1e4370a869ebc991
paulkreft <paulykreft@gmail.com>
2024-01-26 15:15:50 +0100

    add some styles to TW

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "#DADADA"
+      },
+      boxShadow: {
+        card: "0 2px 4px 0 rgba(38, 59, 56, 0.10), 0 0 0 1.5px rgba(38, 50, 56, 0.10)",
+      }
+    },
   },
-  plugins: [],
+  "plugins": ["prettier-plugin-tailwindcss"]
 }
