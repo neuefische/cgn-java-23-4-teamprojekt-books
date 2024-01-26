167b6d97e3f9ca024ad1ed7669d705bb62564a1d
paulkreft <paulykreft@gmail.com>
2024-01-26 15:15:32 +0100

    style book cards

--- frontend/src/components/view-all-books.tsx
+++ frontend/src/components/view-all-books.tsx
@@ -1,17 +1,17 @@
-import {Book} from "../types/Book.ts";
-import {BookCard} from "./book-card.tsx";
+import { Book } from "../types/Book.ts";
+import { BookCard } from "./book-card.tsx";
 import React from "react";
 
 type ViewAllBooksProps = {
-    books: Book[]
-}
+  books: Book[];
+};
 
-export const ViewAllBooks: React.FC<ViewAllBooksProps> = ({books}) => {
-
-    return (
-        <div className="books">
-            {books.map(book => (<BookCard key={book.id} book={book}/>))}
-        </div>
-    );
-
-}
+export const ViewAllBooks: React.FC<ViewAllBooksProps> = ({ books }) => {
+  return (
+    <div className="flex flex-wrap justify-center gap-16">
+      {books.map((book) => (
+        <BookCard key={book.id} book={book} />
+      ))}
+    </div>
+  );
+};
