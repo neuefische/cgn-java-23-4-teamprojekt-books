ba63730ef539abfca4bbaed9714ee747bf7ad2bc
paulkreft <paulykreft@gmail.com>
2024-01-30 08:28:38 +0100

    add main test

--- backend/src/test/java/de/neuefische/team2/backend/BackendApplicationTests.java
+++ backend/src/test/java/de/neuefische/team2/backend/BackendApplicationTests.java
@@ -1,13 +1,20 @@
 package de.neuefische.team2.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BackendApplicationTests {
+    @Autowired
+    ApplicationContext applicationContext;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertNotNull(applicationContext);
+    }
 
 }
