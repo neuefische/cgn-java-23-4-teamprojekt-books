a614c82a9e40d4b88d3a64e543c5a8017eb0b50b
vanessabock <vanessabock54@gmail.com>
2024-01-31 11:46:44 +0100

    Make title in api request optional and filter request with description length

--- backend/src/test/java/de/neuefische/team2/backend/controller/GoogleBooksApiControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/team2/backend/controller/GoogleBooksApiControllerIntegrationTest.java
@@ -158,8 +158,124 @@ public class GoogleBooksApiControllerIntegrationTest {
 
                 //THEN
                 .andExpect(status().isOk())
-                .andExpect(content().json("""
-                        {"description": "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever... • Nominated as one of America’s best-loved novels by PBS’s The Great American Read • “The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.” Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching... A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time."}"""))
+                .andExpect(content().string( "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever... • Nominated as one of America’s best-loved novels by PBS’s The Great American Read • “The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.” Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching... A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time."))
+                .andReturn();
+
+
+        assertEquals(200, mvcResult.getResponse().getStatus());
+    }
+
+    @DirtiesContext
+    @Test
+    void getBookDescriptionTest_WhenIsbn_ThenReturnDescription() throws Exception {
+        //Given
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .setBody("""
+                        {
+                            "kind": "books#volumes",
+                                "totalItems": 2,
+                                "items": [
+                                    {
+                                        "kind": "books#volume",
+                                        "id": "uJOOzwEACAAJ",
+                                        "etag": "4zZBx8ENr7w",
+                                        "selfLink": "https://www.googleapis.com/books/v1/volumes/uJOOzwEACAAJ",
+                                        "volumeInfo": {
+                                            "title": "1984",
+                                            "authors": [
+                                                "George Orwell"
+                                            ],
+                                            "publisher": "Signet",
+                                            "publishedDate": "1950-07-01",
+                                            "description": "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever... • Nominated as one of America’s best-loved novels by PBS’s The Great American Read • “The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.” Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching... A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time.",
+                                            "readingModes": {
+                                                "text": false,
+                                                "image": false
+                                            },
+                                            "maturityRating": "NOT_MATURE",
+                                            "allowAnonLogging": false,
+                                            "contentVersion": "preview-1.0.0",
+                                            "panelizationSummary": {
+                                                "containsEpubBubbles": false,
+                                                "containsImageBubbles": false
+                                            },
+                                            "previewLink": "http://books.google.de/books?id=uJOOzwEACAAJ&dq=isbn:9780451524935+intitle:1984&hl=&as_pt=BOOKS&cd=1&source=gbs_api",
+                                            "infoLink": "http://books.google.de/books?id=uJOOzwEACAAJ&dq=isbn:9780451524935+intitle:1984&hl=&as_pt=BOOKS&source=gbs_api",
+                                            "canonicalVolumeLink": "https://books.google.com/books/about/1984.html?hl=&id=uJOOzwEACAAJ"
+                                        },
+                                        "saleInfo": {
+                                            "country": "DE"
+                                        },
+                                        "accessInfo": {
+                                            "country": "DE",
+                                            "epub": {
+                                                "isAvailable": false
+                                            },
+                                            "pdf": {
+                                                "isAvailable": false
+                                            },
+                                            "accessViewStatus": "NONE"
+                                        },
+                                        "searchInfo": {
+                                            "textSnippet": "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future."
+                                        }
+                                    },
+                                    {
+                                        "kind": "books#volume",
+                                        "id": "d3hA0AEACAAJ",
+                                        "etag": "oky5qu1+uVM",
+                                        "selfLink": "https://www.googleapis.com/books/v1/volumes/d3hA0AEACAAJ",
+                                        "volumeInfo": {
+                                            "title": "1984",
+                                            "authors": [
+                                                "George Orwell"
+                                            ],
+                                            "publisher": "Signet",
+                                            "publishedDate": "1950-07-01",
+                                            "description": "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever... • Nominated as one of America’s best-loved novels by PBS’s The Great American Read • “The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.” Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching... A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time.",
+                                            "readingModes": {
+                                                "text": false,
+                                                "image": false
+                                            },
+                                            "maturityRating": "NOT_MATURE",
+                                            "allowAnonLogging": false,
+                                            "contentVersion": "preview-1.0.0",
+                                            "panelizationSummary": {
+                                                "containsEpubBubbles": false,
+                                                "containsImageBubbles": false
+                                            },
+                                            "previewLink": "http://books.google.de/books?id=d3hA0AEACAAJ&dq=isbn:9780451524935+intitle:1984&hl=&as_pt=BOOKS&cd=2&source=gbs_api",
+                                            "infoLink": "http://books.google.de/books?id=d3hA0AEACAAJ&dq=isbn:9780451524935+intitle:1984&hl=&as_pt=BOOKS&source=gbs_api",
+                                            "canonicalVolumeLink": "https://books.google.com/books/about/1984.html?hl=&id=d3hA0AEACAAJ"
+                                        },
+                                        "saleInfo": {
+                                            "country": "DE"
+                                        },
+                                        "accessInfo": {
+                                            "country": "DE",
+                                            "epub": {
+                                                "isAvailable": false
+                                            },
+                                            "pdf": {
+                                                "isAvailable": false
+                                            },
+                                            "accessViewStatus": "NONE"
+                                        },
+                                        "searchInfo": {
+                                            "textSnippet": "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future."
+                                        }
+                                    }
+                                ]
+                        }"""));
+
+
+        //WHEN
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/api/googleBooks/9780451524935"))
+
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(content().string( "Written more than 70 years ago, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever... • Nominated as one of America’s best-loved novels by PBS’s The Great American Read • “The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.” Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching... A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time."))
                 .andReturn();
 
 
