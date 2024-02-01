ebf9e85a24dcdb186e009203338b85591c3cd89a
paulkreft <paulykreft@gmail.com>
2024-02-01 11:32:45 +0100

    login integration tests

--- backend/src/test/java/de/neuefische/team2/backend/controller/UserControllerIntegrationTest.java
+++ backend/src/test/java/de/neuefische/team2/backend/controller/UserControllerIntegrationTest.java
@@ -4,6 +4,7 @@ import de.neuefische.team2.backend.models.user.User;
 import de.neuefische.team2.backend.repos.user.UsersRepo;
 import de.neuefische.team2.backend.service.IdService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,7 +36,7 @@ class UserControllerIntegrationTest {
 
     @DirtiesContext
     @Test
-    void updateUser_ShouldReturnUpdatedUser() throws Exception {
+    void updateUser_shouldReturnUpdatedUser() throws Exception {
         // Given
         User updatedUser = new User("123", 123, "Paul", new ArrayList<>(), new ArrayList<>());
         when(usersRepo.save(updatedUser)).thenReturn(updatedUser);
@@ -59,15 +60,84 @@ class UserControllerIntegrationTest {
 
         // Verify interactions with mocked dependencies
         verify(usersRepo).save(updatedUser);
-        verifyNoMoreInteractions(usersRepo, idService);
+        verifyNoMoreInteractions(usersRepo);
     }
 
 
     @Test
-    void testGetLoggedInUser_whenLoggedIn_Return() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/user")
-                        .with(oidcLogin().userInfoToken(token -> token.claim("login", "test-user"))))
+    void testGetLoggedInUser_whenNotLoggedIn_returnNull() throws Exception {
+        // Given, when and then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("testBody"));
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    void testGetLoggedInUser_whenReturningUserIsLoggedIn_returnReturningUser() throws Exception {
+        // Given
+        User returningUser = new User("123", 123, "Paul", new ArrayList<>(), new ArrayList<>());
+        when(usersRepo.existsUserByGithubId(returningUser.githubId())).thenReturn(true);
+        when(usersRepo.findUserByGithubId(returningUser.githubId())).thenReturn(returningUser);
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user")
+                        .with(oidcLogin().userInfoToken(token -> token.claim("login", "Paul").claim("id", 123))))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "id":"123",
+                            "githubId":123,
+                            "name":"Paul",
+                            "books":[],
+                            "favouriteBooks":[]
+                        }
+                        """));
+
+        // Then
+        Mockito.verify(usersRepo, Mockito.times(1)).existsUserByGithubId(returningUser.githubId());
+        Mockito.verify(usersRepo, Mockito.times(1)).findUserByGithubId(returningUser.githubId());
+        verifyNoMoreInteractions(usersRepo);
+    }
+
+    @Test
+    void testGetLoggedInUser_whenNewUserIsLoggedIn_returnNewUser() throws Exception {
+        // Given
+        User newUser = new User("123", 123, "Paul", new ArrayList<>(), new ArrayList<>());
+        when(usersRepo.existsUserByGithubId(newUser.githubId())).thenReturn(false);
+        when(usersRepo.save(Mockito.any(User.class))).thenReturn(newUser);
+
+        when(idService.newId()).thenReturn("123");
+
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user")
+                        .with(oidcLogin().userInfoToken(token -> token.claim("login", "Paul").claim("id", 123))))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "id":"123",
+                            "githubId":123,
+                            "name":"Paul",
+                            "books":[],
+                            "favouriteBooks":[]
+                        }
+                        """));
+
+        // Then
+        Mockito.verify(usersRepo, Mockito.times(1)).existsUserByGithubId(newUser.githubId());
+        Mockito.verify(usersRepo, Mockito.times(1)).save(newUser);
+        Mockito.verify(idService, Mockito.times(1)).newId();
+        verifyNoMoreInteractions(usersRepo, idService);
+    }
+
+    @Test
+    void testGetLoggedInUser_whenNoGithubId_returnNull() throws Exception {
+        // When
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/user")
+                        .with(oidcLogin().userInfoToken(token -> token.claim("login", "Paul"))))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+
+        // Then
+        verifyNoInteractions(usersRepo);
     }
 }
